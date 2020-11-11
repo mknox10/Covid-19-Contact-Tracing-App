9d3bde171452980bda4d641197a58213ce638e65
Mitchell Knox <mitchk612@yahoo.com>
2020-11-10 18:08:15 -0600

    location permission initialization

--- app/src/main/java/com/covidcontacttracing/MainActivity.java
+++ app/src/main/java/com/covidcontacttracing/MainActivity.java
@@ -2,7 +2,11 @@ package com.covidcontacttracing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import org.altbeacon.beacon.Beacon;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startBeacon(View view) {
+        /* Create the Activity which will run the Beacon Process. */
+        Intent beacon = new Intent(this, BeaconActivity.class);
+        /* Start the Beacon */
+        startActivity(beacon);
     }
 }
