a17ca0856fff9520d66070deb668a3126146009c
joshreynolds987 <35789044+joshreynolds987@users.noreply.github.com>
2020-11-19 17:44:04 -0600

    test 3

--- app/src/main/java/com/covidcontacttracing/MainActivity.java
+++ app/src/main/java/com/covidcontacttracing/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 
@@ -14,13 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 
     public void startBeacon(View view) {
         /* Create the Activity which will run the Beacon Process. */
         Intent beacon = new Intent(this, BeaconActivity.class);
         /* Start the Beacon */
         startActivity(beacon);
+
+        TextView bttnTxt = (TextView) findViewById(R.id.StartBeaconBttn);
+        bttnTxt.setText("Beacon on");
     }
+
+
+
+
 }
-//test a commit
