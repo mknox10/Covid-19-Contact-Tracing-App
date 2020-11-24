35897c51c62ddd4f2da4ce755b318cb3ca6d4f2f
Mitchell Knox <mitchk612@yahoo.com>
2020-11-24 13:06:06 -0600

    moved beacon activity functionality to beacon service

--- app/src/main/java/com/covidcontacttracing/BeaconService.java
+++ app/src/main/java/com/covidcontacttracing/BeaconService.java
@@ -1,16 +1,13 @@
 package com.covidcontacttracing;
 
 import android.Manifest;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Debug;
 import android.os.IBinder;
-import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,47 +16,61 @@ import androidx.annotation.RequiresApi;
 
 public class BeaconService extends Service {
 
+    Thread beaconThread;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public void onCreate(){
+    public void onCreate() {
         Toast.makeText(this, "Broadcast Service started!", Toast.LENGTH_SHORT).show();
         if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                if (this.checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    // Stop service?
+                if (this.checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // stop the service
+                    // will this work? It will just prevent the thread from being created below? - Mitch
+                    return;
                 }
             }
-        }else{ // Stop service? }
+        } else {
+            // Same thing as line 31
+            return;
 
-        /*createNotificationChannel();
-        Notification builder = new Notification.Builder(this, "chMe")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("Beacon Service")
-                .setContentText("Beacon service is running.")
-                .build();
-        builder.flags |= Notification.FLAG_FOREGROUND_SERVICE;
+            /*
+            * Not sure what this stuff is for? @Brett?
+            */
 
-        startForeground(1995, builder);*/
-        //Get permission for media projection
-        //mediaManager = (MediaProjectionManager)getSystemService(android.content.Context.MEDIA_PROJECTION_SERVICE);
-    }
+            /*createNotificationChannel();
+            Notification builder = new Notification.Builder(this, "chMe")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("Beacon Service")
+                    .setContentText("Beacon service is running.")
+                    .build();
+            builder.flags |= Notification.FLAG_FOREGROUND_SERVICE;
 
-    Thread beaconThread = new Thread(() -> {
-        //Looper.prepare();
-        long startTime = System.currentTimeMillis();
-        while(true){
-            long currentTime = System.currentTimeMillis();
-            //Toast.makeText(this, "Minutes since start: "+((currentTime - startTime) / 60)+" min", Toast.LENGTH_SHORT).show();
-            Log.d("Output", "Minutes since start: "+((currentTime - startTime) / 1000f / 60f)+" min");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            startForeground(1995, builder);*/
+                //Get permission for media projection
+                //mediaManager = (MediaProjectionManager)getSystemService(android.content.Context.MEDIA_PROJECTION_SERVICE);
         }
-    });
+
+
+        //todo: this is where we will need to send/recieve the beacon signal. will this work when the app is off?
+        beaconThread = new Thread(() -> {
+            //Looper.prepare();
+            long startTime = System.currentTimeMillis();
+            while(true){
+                long currentTime = System.currentTimeMillis();
+                // What is this Toast stuff?
+                //Toast.makeText(this, "Minutes since start: "+((currentTime - startTime) / 60)+" min", Toast.LENGTH_SHORT).show();
+                Log.d("Output", "Minutes since start: " + ((currentTime - startTime) / 1000f / 60f) + " min");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        beaconThread.start();
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void createNotificationChannel() {
@@ -87,7 +98,7 @@ public class BeaconService extends Service {
                 DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, imageReader.getSurface(),
                 null, null);
         serverThread.start();*/
-        beaconThread.start();
+//        beaconThread.start();
         return START_STICKY;
     }
 
--- app/src/main/java/com/covidcontacttracing/MainActivity.java
+++ app/src/main/java/com/covidcontacttracing/MainActivity.java
@@ -2,22 +2,89 @@ package com.covidcontacttracing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import org.altbeacon.beacon.Beacon;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Ask permission to use location - Mitch
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    if (this.checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        if (!this.shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setTitle("This app needs background location access");
+                            builder.setMessage("Please grant location access so this app can detect beacons in the background.");
+                            builder.setPositiveButton(android.R.string.ok, null);
+                            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                                @TargetApi(23)
+                                @Override
+                                public void onDismiss(DialogInterface dialog) {
+                                    requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                                            PERMISSION_REQUEST_BACKGROUND_LOCATION);
+                                }
+
+                            });
+                            builder.show();
+                        }
+                        else {
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setTitle("Functionality limited");
+                            builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.");
+                            builder.setPositiveButton(android.R.string.ok, null);
+                            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                                @Override
+                                public void onDismiss(DialogInterface dialog) {
+                                }
+
+                            });
+                            builder.show();
+                        }
+                    }
+                }
+            } else {
+                if (!this.shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                            PERMISSION_REQUEST_FINE_LOCATION);
+                }
+                else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location access to this app.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+            }
+        }
     }
 
     public void startBeacon(View view) {
@@ -32,24 +99,23 @@ public class MainActivity extends AppCompatActivity {
         bttnTxt.setText("Beacon on");
     }
 
-    public void CheckExposure(View view){
-        //check the database fore recent exposure
+    public void CheckExposure(View view) {
+        //todo: check the database fore recent exposure
 
 
         TextView lblExposure = (TextView) findViewById(R.id.lblExposure);
         boolean wasExposed = false;
 
-        if(wasExposed ) {
+        if (wasExposed) {
             lblExposure.setText("You have been exposed please check CDC guiedlines on how to quarantine ");
-        }else{
+        } else{
             lblExposure.setText("You have not been exposed");
         }
 
     }
 
-    public void PositiveResult(View view){
+    public void PositiveResult(View view) {
         //push to the database that you have a positive test result
-
 
     }
 
